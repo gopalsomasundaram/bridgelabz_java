e9d626b012d1ddecc8b11d0d0a3817007c523b2f
gopalsomasundaram <gopal10092004@gmail.com>
2026-01-08 21:16:26 +0530

    D2 progress save

---
+++ ProgrammingElementsProblems/Level1/UniversityDiscountInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class UniversityDiscountInput{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter fee:");
+    int fee = scanner.nextInt();
+    System.out.println("Enter discountPercentage:");
+    int discountPercentage = scanner.nextInt();
+    float discount = ((float)fee/(float)discountPercentage);
+    System.out.println("The discount amount is INR "+discount+" and final discounted fee is INR "+(fee-discount));
+  }
+}
