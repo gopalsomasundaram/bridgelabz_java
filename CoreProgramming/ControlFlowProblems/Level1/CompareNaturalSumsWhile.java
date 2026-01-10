040f8e63fbf4c2d27610dbce89d9c15c7e203708
gopalsomasundaram <gopal10092004@gmail.com>
2026-01-10 15:05:47 +0530

    extra level1 question

---
+++ CoreProgramming/ControlFlowProblems/Level1/CompareNaturalSumsWhile.java
@@ -0,0 +1,27 @@
+package ControlFlowProblems.Level1;
+
+import java.util.Scanner;
+
+public class CompareNaturalSumsWhile {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the natural number:");
+        int n = scanner.nextInt();
+        if(n < 0){
+            System.out.println("Not a natural number");
+            return;
+        }
+        int formula = ((n*(n+1))/2);
+        int manual = 0;
+        int i = 0;
+        while(i<=n){
+            manual+=i;
+            i++;
+        }
+        if(manual == formula){
+            System.out.println("both the results are same "+manual);
+        }else{
+            System.out.println("both the results are different, manual="+manual+" and formula="+formula);
+        }
+    }
+}
